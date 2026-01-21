2aee9f84f6173a1d06a2165986962672ce1cf618
Shy <s.daniell05@outlook.com>
2026-01-21 13:18:18 -0500

    This rock is pissing me off

---
+++ src/SaddlebagBalancer.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class SaddlebagBalancer {
+    /**
+     * Return the authors' names.
+     * @return The names of the authors of this title.
+     */
+    public static String getAuthors() {
+        return "Shy Daniell and Elle Spivey";
+    }
+
+    public static List<Integer> getPartion(List<Integer> packageWeights) {
+
+    }
+}
